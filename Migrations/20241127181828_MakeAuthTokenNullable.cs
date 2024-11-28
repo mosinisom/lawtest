96e0619279d3fac4c532f9177cbce277302b6916
Саша Мосин <alexmosin@inbox.ru>
2024-11-28 11:02:24 +0400

    фикс регистрации

---
+++ Migrations/20241127181828_MakeAuthTokenNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lawtest.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAuthTokenNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,6 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; }
     public string PasswordHash { get; set; }
-    public string AuthToken { get; set; }
+    public string? AuthToken { get; set; }
     public UserRole Role { get; set; }
 }
